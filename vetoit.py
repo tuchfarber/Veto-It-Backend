5d5455d10adf99d5cb11f900790ac68b8759da86
Matt Tuchfarber <matt@tuchfarber.com>
2017-04-03 20:33:32 -0400

    Deletes expired items

--- vetoit.py
+++ vetoit.py
@@ -1,6 +1,7 @@
 from bottle import route, run, get, post, request
 import couchdb
 import uuid
+import time
 
 couch = couchdb.Server("http://couchdb_01:5984")
 
@@ -13,7 +14,7 @@ else:
 def create():
     body = request.json
     d_key = str(uuid.uuid4())
-    data = {"gps":body["gps"],"init_km":body["init_km"],"del_ids":body["del_ids"],"d_key":d_key}
+    data = {"gps":body["gps"],"init_km":body["init_km"],"del_ids":body["del_ids"],"d_key":d_key, "expires":int(time.time()) + 3600}
     (d_id,_) = db.save(data)
     return {"d_id":d_id,"d_key":d_key}
 
@@ -21,19 +22,24 @@ def create():
 def retrieve(d_id,d_key):
     if d_id in db:
         record = db[d_id]
+        if record["expires"] < int(time.time()):
+            db.delete(record)
+            return {"status":"Error", "status_text": "Item has expired"}
         if d_key == record["d_key"]:
-            data = {"gps":record["gps"],"init_km":record["init_km"],"del_ids":record["del_ids"]}
+            return {"gps":record["gps"],"init_km":record["init_km"],"del_ids":record["del_ids"]}
         else:
-            data = {"status":"Error", "status_text": "Provided key invalid"}
+            return {"status":"Error", "status_text": "Provided key invalid"}
     else:
-        data = {"status":"Error", "status_text": "Document does not exist"}
-    return data
+        return {"status":"Error", "status_text": "Document does not exist"}
 
 @post('/vetoit/veto/<d_id>/<d_key>')
 def veto(d_id,d_key):
     body = request.json
     if d_id in db:
         record = db[d_id]
+        if record["expires"] < int(time.time()):
+            db.delete(record)
+            return {"status":"Error", "status_text": "Item has expired"}
         if d_key == record["d_key"]:
             unique_dels = set(body["del_ids"] + record["del_ids"])
             record["del_ids"] = unique_dels
